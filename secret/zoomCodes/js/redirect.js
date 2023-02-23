f8bf5d4ea8dec04826677037178b3269b3bfad12
serhiy <paz.serhiy@gmail.com>
2023-02-23 09:12:08 +0200

    Add some changes

--- secret/zoomCodes/js/redirect.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-let code = localStorage.getItem("classCode")
-if(code){
-  window.location.href = `http://zozrozklad.zzz.com.ua/view/class/${code}/navigation`
-} else {
-  window.location.href = `http://zozrozklad.zzz.com.ua`
-}
