398d5a7221c642637c534081ac15e20c99a5cb3d
serhiy <paz.serhiy@gmail.com>
2023-04-27 10:13:42 +0200

    Add Aspen

---
+++ aspen.cps.edu/aspen/js/database/database.js
@@ -0,0 +1,15 @@
+const STUDENTS = {
+  "SKovalchuk": {
+    password : "Stasik10",
+    firstName: "Stanislav",
+    lastName: "Kovalchuk",
+    grades: {}
+  },
+
+  "Paziuk": {
+    password : "12345678",
+    firstName: "Serhii",
+    lastName: "Paziuk",
+    grades: {}
+  },
+}
